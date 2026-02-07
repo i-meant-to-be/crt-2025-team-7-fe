33b08e005d60d800e7e6a5d9212e5716ba2e3fab
Shawn Kang <shineksu813@outlook.com>
2026-02-08 00:49:06 +0900

    feat: 모달 훅 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,6 +5,7 @@ import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import { initialize, mswLoader } from 'msw-storybook-addon';
 import { allHandlers } from '../src/mocks/handlers/global';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 initialize();
 
@@ -67,20 +68,23 @@ const preview: Preview = {
           ? { pathname: route, state: routeState }
           : { pathname: route },
       ];
+      const queryClient = new QueryClient();
 
       return (
-        <MemoryRouter initialEntries={initialEntries}>
-          {/* routePattern이 지정된 경우에만 <Routes>로 감싸 URL 파라미터를 추출합니다. */}
-          {routePattern ? (
-            <Routes>
-              <Route path={routePattern} element={<Story />} />
-              {/* 혹은 fallback Route도 추가할 수 있습니다 */}
-              <Route path="*" element={<Story />} />
-            </Routes>
-          ) : (
-            <Story />
-          )}
-        </MemoryRouter>
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter initialEntries={initialEntries}>
+            {/* routePattern이 지정된 경우에만 <Routes>로 감싸 URL 파라미터를 추출합니다. */}
+            {routePattern ? (
+              <Routes>
+                <Route path={routePattern} element={<Story />} />
+                {/* 혹은 fallback Route도 추가할 수 있습니다 */}
+                <Route path="*" element={<Story />} />
+              </Routes>
+            ) : (
+              <Story />
+            )}
+          </MemoryRouter>
+        </QueryClientProvider>
       );
     },
   ],
---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,87 @@
+import { ReactNode, useState, useCallback, useEffect } from 'react';
+import { GlobalPortal } from '../utils/GlobalPortal';
+import { CgClose } from 'react-icons/cg';
+
+interface UseModalOptions {
+  closeOnOverlayClick?: boolean;
+  isCloseButtonExist?: boolean;
+  onClose?: () => void;
+}
+
+/**
+ * 모달을 쉽게 열고 닫을 수 있는 훅.
+ * @param options 모달 표시 옵션
+ */
+export function useModal(options: UseModalOptions = {}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const {
+    closeOnOverlayClick = true,
+    isCloseButtonExist = true,
+    onClose = () => {},
+  } = options;
+
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    onClose();
+    setIsOpen(false);
+  }, [onClose]);
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    };
+
+    window.addEventListener('keydown', handleEsc);
+    return () => window.removeEventListener('keydown', handleEsc);
+  }, [closeModal]);
+
+  const handleOverlayClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation();
+      if (e.target === e.currentTarget && closeOnOverlayClick) {
+        closeModal();
+      }
+    },
+    [closeModal, closeOnOverlayClick],
+  );
+
+  const ModalWrapper = ({
+    children,
+    closeButtonColor = 'text-neutral-0 hover:text-gray-300',
+  }: {
+    children: ReactNode;
+    closeButtonColor?: string;
+  }) => {
+    if (!isOpen) return null;
+
+    return (
+      <GlobalPortal.Consumer>
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <div className="relative overflow-hidden rounded-[20px] bg-white shadow-lg">
+            {children}
+            {isCloseButtonExist && (
+              <button
+                type="button"
+                onClick={closeModal}
+                className={`absolute right-4 top-4 text-3xl cursor-pointer ${closeButtonColor}`}
+                aria-label="모달 닫기"
+              >
+                <CgClose className="size-[32px]" />
+              </button>
+            )}
+          </div>
+        </div>
+      </GlobalPortal.Consumer>
+    );
+  };
+
+  return { isOpen, openModal, closeModal, ModalWrapper };
+}
